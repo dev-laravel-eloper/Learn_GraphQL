e59bff88dc7e349e0efd99d32118851c6ce30fa3
Mahdi <mahdidarrudi0930.ha@gmail.com>
2025-09-22 12:36:00 +0330

    Updated Categories Table

--- database/migrations/2025_09_22_085948_create_categories_table.php
+++ database/migrations/2025_09_22_085948_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->nullableMorphs('categorizable');
             $table->string('title');
+            $table->string('slug');
             $table->string('description');
             $table->string('main_image');
             $table->boolean('status')->default(1);
